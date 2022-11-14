123192e4e028d8041df8e1d7d6370f4a55e20d19
19Lida <lida19melnyk@gmail.com>
2022-11-14 13:25:03 +0200

    додає таск 1 і 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+
+// 1.Створення і рендер розмітки.
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkUp = createImgCard(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkUp);
+function createImgCard(images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>`;
+    })
+    .join('');
+}
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick(event){
+    const imageItem = document.querySelector(".gallery__image");
+    event.preventDefault();
+    console.log(imageItem.dataset.source);
+console.log(event.target);
+
+     var lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+        animationSpeed: 250,
+    });
+
+};
+
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    player.on('timeupdate', throttle(timeStop,1000));
+    function timeStop (timeupdate) {
+        let pause = timeupdate.seconds;
+        console.log(pause);
+        localStorage.setItem("videoplayer-current-time" , pause)
 
+    }
+    function afterUpdate(event) {
+        const currentTime = localStorage.getItem("videoplayer-current-time");
+        player.setCurrentTime(currentTime).then(function(pause){
+
+        }).catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                    // the time was less than 0 or greater than the video’s duration
+                    break;
+        
+                default:
+                    // some other error occurred
+                    break;
+            }
+        
+    });
+}

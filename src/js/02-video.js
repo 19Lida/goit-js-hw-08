e8fef68f02ccb2bf5b085a61beeedf0bb8dfd253
19Lida <lida19melnyk@gmail.com>
2022-11-16 01:12:12 +0200

    редагує 2 і 3 таск

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,26 +3,26 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
     player.on('timeupdate', throttle(timeStop,1000));
-    function timeStop (timeupdate) {
-        let pause = timeupdate.seconds;
-        console.log(pause);
-        localStorage.setItem("videoplayer-current-time" , pause)
+    function timeStop (event) {
+        // let pause = timeupdate.seconds;
+        // console.log(pause);
+        localStorage.setItem("videoplayer-current-time" , event.seconds);
 
     }
-    function afterUpdate(event) {
+    // function afterUpdate(event) {
         const currentTime = localStorage.getItem("videoplayer-current-time");
-        player.setCurrentTime(currentTime).then(function(pause){
+    //     player.setCurrentTime(currentTime).then(function(pause){
 
-        }).catch(function(error) {
-            switch (error.name) {
-                case 'RangeError':
-                    // the time was less than 0 or greater than the video’s duration
-                    break;
+    //     }).catch(function(error) {
+    //         switch (error.name) {
+    //             case 'RangeError':
+    //                 // the time was less than 0 or greater than the video’s duration
+    //                 break;
         
-                default:
-                    // some other error occurred
-                    break;
-            }
+    //             default:
+    //                 // some other error occurred
+    //                 break;
+    //         }
         
-    });
-}
+    // });
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,42 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 // ключ до сховища
 const STORAGE_KEY = 'feedback-form-state';
 
 // отримуємо доступ до елементів форми
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('feedback-form textarea'),
-    input: document.querySelector('feedback-form input'),
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('feedback-form textarea'),
+  input: document.querySelector('feedback-form input'),
 };
 
 // додаємо значення
-let formData ={};
+let formData = {};
 // додаємо функціонал
 onFormState();
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextareaInput,500));
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 // функція збереження поточних значень
 function onTextareaInput(event) {
-    event.preventDefault();
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    
+  event.preventDefault();
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 // функція перевірки стану сховища
 function onFormState() {
-    const messageStoregeSave = localStorage.getItem(STORAGE_KEY);
-    const parseStoregeMessage = JSON.parse(messageStoregeSave);
-    if (messageStoregeSave) {
-        (refs.textarea.value = parseStoregeMessage.message || "");
-        (refs.input.value = parseStoregeMessage.email || "");
-    }
+  const data = localStorage.getItem(STORAGE_KEY);
+  const formData = JSON.parse(messageStoregeSave);
+  if (data) {
+    refs.textarea.value = formData.message || '';
+    refs.input.value = formData.email || '';
+  }
 }
 // очищає сабміт,встановлюємо по замовчуванню поведінку,очищаємо форму,очищаємо сховище
 function onFormSubmit(event) {
-    event.preventDefault();
-    // очищення форми після відправки
-    event.currentTarget.reset(); 
-// очищення локалсторедж після відправки смс
-    localStorage.removeItem(STORAGE_KEY);
-    
+  event.preventDefault();
+  // очищення форми після відправки
+  event.currentTarget.reset();
+  // очищення локалсторедж після відправки смс
+  localStorage.removeItem(STORAGE_KEY);
 }

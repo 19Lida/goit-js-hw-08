affb96e3c76ed5e0fd259711ebd90a8fb48028c5
19Lida <lida19melnyk@gmail.com>
2022-11-24 23:30:37 +0200

    редагує завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,11 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
-    player.on('timeupdate', throttle(timeStop,1000));
-    function timeStop (event) {
-        // let pause = timeupdate.seconds;
-        // console.log(pause);
-        localStorage.setItem("videoplayer-current-time" , event.seconds);
-
-    }
-    // function afterUpdate(event) {
-        const currentTime = localStorage.getItem("videoplayer-current-time");
-    //     player.setCurrentTime(currentTime).then(function(pause){
-
-    //     }).catch(function(error) {
-    //         switch (error.name) {
-    //             case 'RangeError':
-    //                 // the time was less than 0 or greater than the video’s duration
-    //                 break;
-        
-    //             default:
-    //                 // some other error occurred
-    //                 break;
-    //         }
-        
-    // });
+const player = new Player(iframe);
+player.on('timeupdate', throttle(timeStop, 1000));
+function timeStop(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+const currentTime = localStorage.getItem('videoplayer-current-time') || '0';
+//
 player.setCurrentTime(currentTime);
